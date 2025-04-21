a0c1570b70427136264f82d546967ce23533bcfb
abhishek kamat <abhishekkamat142@gmail.com>
2025-04-22 00:01:40 +0530

    learn some string function

--- second_day/string.py
+++ second_day/string.py
@@ -9,3 +9,14 @@ print(chai)
 chai = "Lemon, Ginger, Masala, Mint"
 print(chai.split(", "))
 
+abhi = "Abhishek Kumar Kamat"
+print(abhi.split(" "))
+# here split function convert string into array based on " "
+print("".join(abhi))
+# here join function convert array into string based on "" and space between all the values;
+print(len(abhi))
+myDict = {"name": "John", "country": "Norway"}
+mySeparator = "TEST"
+x = mySeparator.join(myDict)
+print(x)
+
